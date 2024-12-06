a14cdb6459ef105004970b43073c7c767cca9c76
ahsanjamali <ahsanjamali7042@gmail.com>
2024-12-06 21:15:56 +0500

    Improve code 1

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Connect to MongoDB
